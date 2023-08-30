ce6ec9b8c2228ce283d2f719fe9f2ddb83bf4a66
ofcpire <ofcpire@gmail.com>
2023-08-31 03:54:24 +0900

    Feat : Global 컴포넌트

---
+++ src/components/global/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header style={{ border: '1px solid black', padding: '30px', textAlign: 'center' }}>
+      facebook / react
+    </header>
+  );
+}
