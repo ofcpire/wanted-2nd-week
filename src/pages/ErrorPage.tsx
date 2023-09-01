4b5b50afed57c8c61250152131143e2dc5ded3a2
ofcpire <ofcpire@gmail.com>
2023-09-01 15:56:20 +0900

    Refactor : useNavigate 사용

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ErrorPage() {
+  const navigate = useNavigate();
+  const navToList = () => navigate('/');
+
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-semibold p-5">Not found!</h1>
-      <a href="/">리스트로 돌아가기</a>
+      <h1 className="text-2xl font-semibold p-1">Not found!</h1>
+      <button onClick={navToList}>리스트로 돌아가기</button>
     </section>
   );
 }
